2cf562c9efa4a902f6f572c020a8d84367581e98
morrisedu <eduumorris@gmail.com>
2022-09-07 19:54:44 +0300

    Abstract comment removed

--- src/main/java/OOPReview/ObjReview.java
+++ src/main/java/OOPReview/ObjReview.java
@@ -5,15 +5,4 @@ public class ObjReview {
         Male ilya = new Male(); // Add variables here
         Female oksana = new Female(); // Add the variables here
     }
-
-    /*
-        Functions (Methods)
-        Abstract method
-        Talk (Erwa)
-        Walk (Elena)
-        Eat (Nonna)
-        Sleep (Aysel)
-        Learn (Ilya)
-
-    */
 }
