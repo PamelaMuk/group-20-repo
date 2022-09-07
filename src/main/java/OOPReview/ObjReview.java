2aefdd4d672df2263e4048f7c76997c87be29b4a
morrisedu <eduumorris@gmail.com>
2022-09-07 19:17:26 +0300

    OOP review assignment one

---
+++ src/main/java/OOPReview/ObjReview.java
@@ -0,0 +1,19 @@
+package OOPReview;
+
+public class ObjReview {
+    public static void main(String[] args) {
+        Male ilya = new Male(); // Add variables here
+        Female oksana = new Female(); // Add the variables here
+    }
+
+    /*
+        Functions (Methods)
+        Abstract method
+        Talk (Erwa)
+        Walk (Elena)
+        Eat (Nonna)
+        Sleep (Aysel)
+        Learn (Ilya)
+
+    */
+}
