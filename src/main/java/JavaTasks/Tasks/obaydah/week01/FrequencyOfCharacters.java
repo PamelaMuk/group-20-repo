cd090fb4ed2584feee3bb5407323fc9d512f2513
Obaydah Moussa <o619moussa@gmail.com>
2022-09-21 18:58:29 -0400

    Week01 java project

---
+++ src/main/java/JavaTasks/Tasks/obaydah/week01/FrequencyOfCharacters.java
@@ -0,0 +1,9 @@
+package JavaTasks.Tasks.obaydah.week01;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+        String str = "AAASEEEERRRRRTTII";
+        
+    }
+}
