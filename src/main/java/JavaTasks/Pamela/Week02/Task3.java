9dd91bac6c9448821d5e61028db6a3bd99d217dc
PamelaMuk <ulupiquita@gmail.com>
2022-10-04 23:05:21 -0400

    week02 Task3

---
+++ src/main/java/JavaTasks/Pamela/Week02/Task3.java
@@ -0,0 +1,15 @@
+package JavaTasks.Pamela.Week02;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 15;
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("After swapping: "+"a = "+ a +", b = "+ b);
+    }
+}
