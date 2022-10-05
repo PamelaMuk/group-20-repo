e91c148866c1e8b1dd2f40fbf69543606f000697
PamelaMuk <ulupiquita@gmail.com>
2022-10-04 21:10:09 -0400

    week02 Task1

---
+++ src/main/java/JavaTasks/Pamela/Week02/Task1.java
@@ -0,0 +1,22 @@
+package JavaTasks.Pamela.Week02;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int div,result=0;
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter the number you want to be divide by: ");
+        int num2 = input.nextInt();
+
+        while (num1>=num2){
+            num1 = num1 - num2;
+            result++;
+        }
+        System.out.println("Division is: "+ result);
+    }
+}
